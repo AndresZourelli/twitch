9830edd6dd5285aa17d6c3e945cd2674849e7910
Andres <azourelli@gmail.com>
2018-08-20 18:53:24 -0500

    overlay feature maybe?

--- src/Main/Header/Header.js
+++ src/Main/Header/Header.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import streamerBlue from './streamerBlue.png'
+import streamerBlue from './streamerBlue.png';
+import streamerPurple from './streamerPurple.png';
+import streamerOrange from './streamerOrange.png';
+import { Container, Row, Col } from 'reactstrap';
 import {
   Carousel,
   CarouselItem,
@@ -7,22 +10,18 @@ import {
   CarouselIndicators,
   CarouselCaption
 } from 'reactstrap';
-
 import './Header.css';
 
 const items = [
   {
-    src: {streamerBlue}
+    src:  `${streamerBlue}`,
+    caption: "Streamer Human" 
   },
   {
-    src: 'text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-    altText: 'Slide 2',
-    caption: 'Slide 2'
+    src: `${streamerPurple}`
   },
   {
-    src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-    altText: 'Slide 3',
-    caption: 'Slide 3'
+    src: `${streamerOrange}`
   }
 ];
 
@@ -73,6 +72,16 @@ class Header extends Component {
           key={item.src}
         >
           <img src={item.src} alt={item.altText} />
+          {<Container className="overlay_col">
+          <Row>
+            <Col  sm = "8">
+            hi
+            </Col>
+            <Col  sm="4">
+              Hello
+            </Col>
+          </Row>
+        </Container>}
           <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
         </CarouselItem>
       );
@@ -84,10 +93,27 @@ class Header extends Component {
         next={this.next}
         previous={this.previous}
       >
+       
         <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+
         {slides}
+        
+        
+          
         <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
         <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+        {<Container className="overlay_col">
+          <Row>
+            <Col  sm = "8">
+            hi
+            </Col>
+            <Col  sm="4">
+              Hello
+            </Col>
+          </Row>
+        </Container>}
+        
+
       </Carousel>
     );
   }
