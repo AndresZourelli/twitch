1350bdc24042b646fc9ab6037db8967bbec297f1
Andres <azourelli@gmail.com>
2018-09-03 17:26:21 -0400

    post for signup works and hash works

--- server.js
+++ server.js
@@ -5,12 +5,25 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const bcrypt = require("bcrypt");
 require('dotenv').load();
-
+const checkAuth = require('./server/check-auth');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+const database = {
+  users: [{
+    _id: '123',
+    name: 'Bob',
+    email: 'bob@gmail.com',
+    joined: new Date()
+  }],
+  secrets: {
+    users_id: '123',
+    hash: 'wha'
+  }
+}
 
 //Talk to .env file testing area
 // console.log('No value for FOO yet:', process.env.JWT_KEY);
@@ -67,22 +80,72 @@ app.use(cors());
 app.use(morgan('combined'));
 
 
-const checkJwt = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://streamingsociety.auth0.com/.well-known/jwks.json`
-  }),
-
-    audience: 'uhG1WInIUpM5ghf76s9X1pLmZrnshQir',
-  issuer: `https://streamingsociety.auth0.com`,
-  algorithms: ['RS256']
-});
-
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
 
 app.listen(port, () => console.log(`Listening on port hi ${port}`));
+
+app.post("/signup", (req, res) => {
+    if (database.users[0].email === req.body.email) {
+    return res.status(409).json({
+      message: 'User Exists'
+    });
+  } else {
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+      if (err) {
+        return res.status(500).json({
+          error: err
+        });
+      } else {
+        const user =({
+          _id: "1",
+          email: req.body.email,
+          password: hash
+        });
+        return res.status(200).json({
+          message: user
+        });
+      }
+    })
+  }
+});
+
+
+
+
+
+app.post('/login', (req, res, next) => {
+  User.find({ email: req.body.email })
+  .exec()
+  .then(user =>{
+    if (user.length < 1){
+      return res.status(401).json({
+        message: 'Auth Failed'
+      });
+    }
+    bcrypt.compare(req.body.password, user[0].password, (err, result) =>{
+      if (err) {
+        return res.status(401).json({
+          message: 'Auth Failed'
+        });
+      }
+      if (result) {
+        return res.status(200).json({
+          message: 'Auth successful'
+        });
+      }
+      res.status(401).json({
+        message: 'Auth Failed'
+      });
+    });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      message: err
+    });
+  })
+});
+
