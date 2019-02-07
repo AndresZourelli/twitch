03a536ec0f6a4a1a789503197e5375cf049dd1c6
Andres <azourelli@gmail.com>
2019-02-06 19:36:11 -0600

    new update

--- client/src/Main/Equip/Equip.js
+++ client/src/Main/Equip/Equip.js
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import "./Equip.css";
 const Equip = () => {
 	return(
-		<div>Equip</div>
+		<div className="text-format">
+			<h1><strong>What do you need to get started streaming?</strong></h1>
+				<p>
+					
+				</p>
+		</div>
 		);
 };
 
